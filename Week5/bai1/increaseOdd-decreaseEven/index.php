cd48e859b66e5a7f5e313beb14b80c2478001da4
6thanh <sauthanh106@gmail.com>
2021-11-16 05:56:37 +0700

    add w4-5

---
+++ Week5/bai1/increaseOdd-decreaseEven/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Increase Odd - Decrease Even</title>
+</head>
+<body>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<input type="text">
+<input type="submit" name="submit" value="Submit">
+</form>
+</body>
+</html>
